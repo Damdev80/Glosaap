2942451a6b7ad59f1eab14a325b11e552d1cc972
Damdev80 <danielbuelvas9624@gmail.com>
2025-12-01 11:03:18 -0500

    feat: Add main package docstring and update import path in config module

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,5 +1,5 @@
 """
 Configuración de la aplicación
 """
-from config.eps_config import EPS_CONFIG, get_eps_by_name, get_all_eps
+from app.config.eps_config import EPS_CONFIG, get_eps_by_name, get_all_eps
 
