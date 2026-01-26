5d8a84960f3db994a7a70c004631a51be140bae7
Damdev80 <danielbuelvas9624@gmail.com>
2026-01-26 15:21:12 -0500

    feat: Prueba

--- app/config/settings.py
+++ app/config/settings.py
@@ -12,7 +12,7 @@ from pathlib import Path
 
 # Versión actual de la aplicación (seguir semver: MAJOR.MINOR.PATCH)
 # Actualizar manualmente en cada release
-APP_VERSION = "1.0.1"
+APP_VERSION = "1.0.2"
 
 # Nombre de la aplicación
 APP_NAME = "Glosaap"
